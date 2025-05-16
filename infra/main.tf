ab5b4870b748a1a8c2fecc55a953576dc5f2f3f3
Paul Hinz <paul.hinz@haw-hamburg.de>
2025-05-16 21:56:53 +0200

    fixed CD workflow to Terraform with bucket

--- infra/main.tf
+++ infra/main.tf
@@ -2,6 +2,16 @@ provider "aws" {
   region = var.aws_region
 }
 
+terraform {
+  backend "s3" {
+    bucket         = "uptime-monitor-paul"
+    key            = "uptime-monitor/terraform.tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
+  }
+}
+
 resource "aws_elastic_beanstalk_application" "app" {
   name        = var.app_name
   description = "Uptime Monitor App"
