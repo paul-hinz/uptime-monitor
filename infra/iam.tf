0e6f7bfcac30222d752a8857dfb958d4eef6f7b5
Paul Hinz <paul.hinz@haw-hamburg.de>
2025-05-16 21:26:54 +0200

    adjusted CD workflow to Terraform

---
+++ infra/iam.tf
@@ -0,0 +1,24 @@
+resource "aws_iam_role" "beanstalk_ec2_role" {
+  name = "beanstalk-ec2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "beanstalk_ec2_policy_attach" {
+  role       = aws_iam_role.beanstalk_ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkWebTier"
+}
+
+resource "aws_iam_instance_profile" "beanstalk_instance_profile" {
+  name = "beanstalk-instance-profile"
+  role = aws_iam_role.beanstalk_ec2_role.name
+}
